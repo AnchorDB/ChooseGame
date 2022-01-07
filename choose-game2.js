395b9b8ac943a8196be9cb657f9de417ecda1d2e
Dan <Oceanside31@gmail.com>
2022-01-06 22:18:55 -0500

    added comments on progress

--- choose-game2.js
+++ choose-game2.js
@@ -1,12 +1,18 @@
 let gameList = [];
 
+// added event listener to form submit
+
 document.getElementById('formSubmit').addEventListener('submit', addGame);
+
+// add submitted game to games array
 
 function addGame(e) {
    games = document.getElementById("gameToAdd").value;
    gameList.push(games);
    e.preventDefault();
 }
+
+// choose random game from games array
 
 function chooseGame() {
     const randomGame = gameList[Math.floor(Math.random() * gameList.length)];
