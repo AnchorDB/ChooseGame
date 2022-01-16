aa7233ba7144a093c3bc2a4c540746a98f02ace5
Dan <Oceanside31@gmail.com>
2022-01-16 14:31:32 -0500

    moved functions into class prototypes

--- choose-game2.js
+++ choose-game2.js
@@ -1,52 +1,44 @@
-//variables
+// classes
 
 class GameList {
     constructor(gameList = []) {
         this.gameList = gameList;
     } 
 
-    // addGame(e) {
-    //     const gameList = new GameList;
-    //     gameList.push(gameToAdd.value);
-    //     gameToAdd.value = '';
-    //     e.preventDefault();
-    //  }
+    addGame(e) {
+        let gameToAdd = document.getElementById("gameToAdd");
+
+        games.gameList.push(gameToAdd.value);
+        gameToAdd.value = '';
+        e.preventDefault();
+     }
+
+     chooseGame() {
+        let submittedGame = document.getElementById("game");
+        let randomGame = games.gameList[Math.floor(Math.random() * games.gameList.length)];
+    
+        // check if game has been entered
+        if (games.gameList.length !== 0){
+            setTimeout(function() {
+                return submittedGame.innerHTML = randomGame;},
+                500);
+        } else {
+            submittedGame.innerHTML = "Please Enter a Game Above";
+        }
+    }   
+
+    reloadList(){
+        window.location.reload();
+    }
+
 }
-let gameToAdd = document.getElementById("gameToAdd");
-let submittedGame = document.getElementById("game");
+
+// instantiate
+
 const games = new GameList([]);
 
-// added event listener to form submit, new list submit
+// Event Listeners
 
-document.getElementById('formSubmit').addEventListener('submit', addGame);
-document.querySelector('#reloadPage').addEventListener('mousedown', reloadList);
-
-// add submitted game to games array
-
-function addGame(e) {
-   games.gameList.push(gameToAdd.value);
-   gameToAdd.value = '';
-   e.preventDefault();
-}
-
-// choose random game from gamelist array
-
-function chooseGame() {
-    let randomGame = games.gameList[Math.floor(Math.random() * games.gameList.length)];
-
-    // check if game has been entered
-    if (games.gameList.length !== 0){
-        setTimeout(function() {
-            return submittedGame.innerHTML = randomGame;},
-            500);
-    } else {
-        submittedGame.innerHTML = "Please Enter a Game Above";
-    }
-}
-
-// create new List
-
-function reloadList(){
-    window.location.reload();
-}
+document.getElementById('formSubmit').addEventListener('submit', games.addGame);
+document.querySelector('#reloadPage').addEventListener('mousedown', games.reloadList);
 
